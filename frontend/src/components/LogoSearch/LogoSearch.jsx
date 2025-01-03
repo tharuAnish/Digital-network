6184752caff8d2c684414e662eaec15d5ab7856f
tharuAnish <namoanishtharu@gmail.com>
2025-01-03 22:45:06 +0530

    With initializing other component , Home page done

---
+++ frontend/src/components/LogoSearch/LogoSearch.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function LogoSearch() {
+  return <div>LogoSearch</div>
+}
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,7 +1,24 @@
-import React from "react"
+import React, { useEffect } from "react"
+import PostSide from "../../components/PostSide/PostSide"
+import ProfileSide from "../../components/profileSide/ProfileSide"
+import RightSide from "../../components/RightSide/RightSide"
 
+import "./Home.css"
+import { useNavigate } from "react-router-dom"
 const Home = () => {
-  return <div>Home</div>
+  const navigate = useNavigate()
+  // useEffect(() => {
+  //   if (!localStorage.getItem("userId")) {
+  //     navigate("/")
+  //   }
+  // }, [])
+  return (
+    <div className="Home">
+      <ProfileSide />
+      <PostSide />
+      <RightSide />
+    </div>
+  )
 }
 
 export default Home
