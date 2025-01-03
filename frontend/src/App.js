e931689e98d8cea6afea837ef741940b40511855
tharuAnish <namoanishtharu@gmail.com>
2025-01-03 19:34:50 +0530

    Auth signup fe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css"
 import { Auth, SignUp } from "./pages/Auth/Auth"
 import Home from "./pages/home/Home"
 import Profile from "./pages/Profile/Profile"
+
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
